c52af5c88a4ce070a4f63a066bc875e16c01c497
Nandadeep Davuluru <aditya.nandadeep@gmail.com>
2018-02-20 17:48:23 -0800

    image downloader complete

--- images.py
+++ images.py
@@ -1,19 +1,13 @@
 import urllib.request
-import cv2, re, pandas
+import cv2, pandas
 
-regex = r'.*\/(.*)\.jpg'
-url = 'http://mw2.google.com/mw-panoramio/photos/medium/60760466.jpg'
-urllib.request.urlretrieve(url, "file_name.jpg")
-getImage = lambda url : urllib.request.urlretrieve(url, filnam)
+getImage = lambda url, name : urllib.request.urlretrieve(url, str(name) + str('.jpg'))
 images = pandas.read_csv('index.csv')
 
 def main():
 
 	for index, row in images.iterrows():
-		# print(index, row[1])
-
-		# call getImage for row[1] here and it'll write to the file automatically. to get name, use the regex
-
-
+		getImage(row[1], index)
+		
 if __name__ == '__main__':
 	main()
